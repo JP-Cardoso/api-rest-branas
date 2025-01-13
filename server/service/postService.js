1bffa1d8c82a4dff6b55d87d9b36de6259ba7792
jp-cardoso <jhonpoal@hotmail.com>
2025-01-12 22:37:41 -0300

    Reapply "feat: adding new methods to the API"
    This reverts commit 56339ebce0a1c9f8a144f3366997f7f3c76749d8.

--- server/service/postService.js
+++ server/service/postService.js
@@ -4,11 +4,19 @@ exports.getPosts = function () {
   return postData.getPosts();
 }
 
-exports.getPostsById = function (id) {
-  return postData.getPostsById(id);
+exports.getPostsById = async function (id) {
+  const post = await postData.getPostsById(id);
+
+  if(!post) throw new Error("Post not found");
+
+  return post;
 }
 
-exports.savePosts = function (post) {
+exports.savePosts = async function (post) {
+  const existingPost = await postData.getPostsByTitle(post.title);
+
+  if(existingPost) throw new Error("Post already exists");
+
   return postData.savePosts(post);
 }
 
@@ -16,6 +24,7 @@ exports.deletePosts = function (id) {
   return postData.deletePosts(id);
 }
 
-exports.updatePosts = function (id, data) {
+exports.updatePosts = async function (id, data) {
+  await exports.getPostsById(id)
   return postData.updatePosts(id, data);
 }
