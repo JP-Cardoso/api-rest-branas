601ffdc93620b87ae4f57b75fb97d0ea071a765a
jp-cardoso <jhonpoal@hotmail.com>
2025-01-12 22:38:33 -0300

    feat: adding a new route test

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -7,7 +7,7 @@ const generate = function () {
 }
 
 const request = function ({ url, method, data }) {
-  return axios({ url, method, data });
+  return axios({ url, method, data, validateStatus: false });
 }
 
 test("should get posts", async () => {
@@ -36,13 +36,14 @@ test("should get posts", async () => {
   //then - então
   const posts = response.data;
   expect(posts).toHaveLength(3);
+  expect(response.status).toBe(200)
 
   await postService.deletePosts(post1.id);
   await postService.deletePosts(post2.id);
   await postService.deletePosts(post3.id);
 })
 
-test("should save post", async () => {
+test("should save s post", async () => {
   //given - dado que
   const data = {
     title: generate(),
@@ -60,7 +61,34 @@ test("should save post", async () => {
   const post = response.data;
   expect(post.title).toBe(data.title);
   expect(post.content).toBe(data.content);
+  expect(response.status).toBe(201)
   await postService.deletePosts(post.id);
+})
+
+test("should not save a post", async () => {
+  //given - dado que
+  const data = {
+    title: generate(),
+    content: generate(),
+  };
+
+  //when - quando acontecer
+  const response1 = await request({
+    url: "http://localhost:3000/posts",
+    method: "POST",
+    data
+  });
+
+  const response2 = await request({
+    url: "http://localhost:3000/posts",
+    method: "POST",
+    data
+  });
+
+  //then - então
+  const post1 = response1.data;
+  expect(response2.status).toBe(409);
+  await postService.deletePosts(post1.id);
 })
 
 test("should update a post", async () => {
@@ -74,18 +102,36 @@ test("should update a post", async () => {
   post.content = generate();
 
   //when - quando acontecer
-  await request({
+  const response = await request({
     url: `http:localhost:3000/posts/${post.id}`,
     method: "PUT",
     data: post
   })
 
   const updatedPost = await postService.getPostsById(post.id);
-  
+
   //then - então
   expect(updatedPost.title).toBe(post.title);
   expect(updatedPost.content).toBe(post.content);
+  expect(response.status).toBe(204);
   await postService.deletePosts(post.id);
+})
+
+test("should not update a post", async () => {
+  //given - dado que
+  const post = {
+    id: 1
+  }
+
+  //when - quando acontecer
+  const response = await request({
+    url: `http:localhost:3000/posts/${post.id}`,
+    method: "PUT",
+    data: post
+  })
+
+  //then - então
+  expect(response.status).toBe(404)
 })
 
 test("should delete a post", async () => {
@@ -96,7 +142,7 @@ test("should delete a post", async () => {
   });
 
   //when - quando acontecer
-  await request({
+  const response = await request({
     url: `http:localhost:3000/posts/${post.id}`,
     method: "DELETE",
   })
@@ -104,4 +150,5 @@ test("should delete a post", async () => {
   //then - então
   const posts = await postService.getPosts();
   expect(posts).toHaveLength(0);
+  expect(response.status).toBe(204);
 })
